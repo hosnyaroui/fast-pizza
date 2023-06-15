3b452800f341f6df00429610a3301c338988c01c
hosnyaroui <hosny.cris@gmail.com>
2023-06-15 22:31:12 +0100

    Fetching Data With React Router Loaders Pizza Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
 import Home from "./user interface/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader} from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -18,7 +18,8 @@ children: [
   },
   {
     path: "/menu",
-    element: <Menu />
+    element: <Menu />,
+    loader: menuLoader,
   },
   {
     path: '/cart', element: <Cart />
