2ddf732ce91b417a355cc60ec7731c55c7f7eb2a
hosnyaroui <hosny.cris@gmail.com>
2023-06-19 22:33:21 +0100

    Reusing Styles With React Components

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../user interface/Button";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -49,13 +50,13 @@ const formErrors = useActionData();
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input className="input" type="text" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input className="input" type="tel" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -63,7 +64,7 @@ const formErrors = useActionData();
         <div>
           <label>Address</label>
           <div>
-            <input className="rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 w-full md:px-6 md:py-2" type="text" name="address" required />
+            <input className="input" type="text" name="address" required />
           </div>
         </div>
 
@@ -80,7 +81,7 @@ const formErrors = useActionData();
 
         <div>
         <input  type="hidden" name='cart' value={JSON.stringify(cart)}/>
-          <button disabled={isSubmitting} className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 active:bg-slate-400 disabled:cursor-not-allowed"> {isSubmitting ? "Placing order...." : "Order now"}</button>
+          <Button disabled={isSubmitting} > {isSubmitting ? "Placing order...." : "Order now"}</Button>
         </div>
       </Form>
     </div>
