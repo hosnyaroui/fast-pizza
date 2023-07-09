40b7c6d95d65279262ccb565096465117dd3b5d3
hosnyaroui <hosny.cris@gmail.com>
2023-07-09 13:29:23 +0100

    Fetching Data Without Navigation useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import OrderItem from './OrderItem';
 import {
@@ -8,11 +8,22 @@ import {
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
 
 
 
 function Order() {
 const order = useLoaderData();
+
+const fetcher = useFetcher();
+
+useEffect(function() {
+
+if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+
+},
+[fetcher]);
+
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -47,7 +58,9 @@ const order = useLoaderData();
       </div>
 
 <ul className="divide-y divide-stone-200 border-b border-t">
-  {cart.map((item)=> (<OrderItem item={item} key={item.pizzaId}/>))}
+  {cart.map((item)=> (<OrderItem item={item} key={item.pizzaId} 
+  isLoadingIngredients={fetcher.state === 'loading'}
+  ingredients={fetcher?.data?.find((el) => el.id === item.pizzaId)?.ingredients ?? [] }/>))}
 </ul>
 
       <div className="space-y-2 bg-stone-200 py-5 px-6">
--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -6,13 +6,14 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
   
 
   return (
-    <li className="py-3">
+    <li className="py-3 space-y-1">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
+      <p className="text-sm capitalize italic text-stone-500"> {isLoadingIngredients ? 'loading...' : ingredients.join(', ')} </p>
     </li>
   );
 }
